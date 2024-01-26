fe469a16ec10a488940a345a9914470d448cafbd
Ihor Karen <karenihor@gmail.com>
2024-01-26 11:17:40 +0200

    layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,74 @@
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <AppBar
+      position="fixed"
+      color="default"
+      component="header"
+      elevation={0}
+      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+    >
+      <Toolbar sx={{ flexWrap: "wrap" }}>
+        <Typography
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1, textAlign: "start" }}
+        >
+          Company name
+        </Typography>
+        <nav>
+          <Link
+            component={NavLink}
+            to="/home"
+            variant="button"
+            color="text.primary"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Home
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Enterprise
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Support
+          </Link>
+        </nav>
+        <Button
+          component={NavLink}
+          to="/auth/login"
+          variant="outlined"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Login
+        </Button>
+        <Button
+          component={NavLink}
+          to="/auth/register"
+          variant="outlined"
+          sx={{ my: 1, mx: 1.5 }}
+        >
+          Register
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
