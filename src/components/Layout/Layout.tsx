6e1ab9ad014da078bbcdd14e193d243593d2957f
Ihor Karen <karenihor@gmail.com>
2024-02-04 11:57:40 +0200

    hero variant

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Container from "@mui/material/Container";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
-    <Container>
+    <>
       <Header />
       <Suspense fallback={<div>Loading...</div>}>
         <main>
@@ -14,7 +13,7 @@ const Layout = () => {
         </main>
       </Suspense>
       <Footer />
-    </Container>
+    </>
   );
 };
 
